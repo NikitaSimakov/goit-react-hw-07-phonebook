5b5c2e489785c4a3de38a82bbd90caa414459825
NikitaSimakov <106473909+NikitaSimakov@users.noreply.github.com>
2023-06-18 22:03:30 +0300

    Update contactSlice.js

--- src/redux/contactSlice.js
+++ src/redux/contactSlice.js
@@ -3,6 +3,11 @@ import { createSlice } from '@reduxjs/toolkit';
 import { fetchContacts, deleteContact, addContact } from './thunks';
 // import { Notify } from 'notiflix';
 
+const handlePending = state => (state.isLoading = true);
+const handleRejected = (state, { payload }) => {
+  state.error = payload;
+  state.isLoading = false;
+};
 const contactsSlice = createSlice({
   name: 'contacts',
   initialState: contactsState,
@@ -12,51 +17,23 @@ const contactsSlice = createSlice({
         state.contacts = payload;
         state.isLoading = false;
       })
-      .addCase(fetchContacts.pending, state => {
-        state.isLoading = true;
-      })
-      .addCase(fetchContacts.rejected, (state, { payload }) => {
-        state.error = payload;
-        state.isLoading = false;
-      })
+      .addCase(fetchContacts.pending, handlePending)
+      .addCase(fetchContacts.rejected, handleRejected)
       .addCase(deleteContact.fulfilled, (state, { payload }) => {
         const index = state.contacts.findIndex(
           contact => contact.id === payload
         );
         state.contacts.splice(index, 1);
       })
+      .addCase(deleteContact.pending, handlePending)
+      .addCase(deleteContact.rejected, handleRejected)
       .addCase(addContact.fulfilled, (state, { payload }) => {
         console.log(payload);
         state.contacts.push(payload.data);
-      });
+      })
+      .addCase(addContact.pending, handlePending)
+      .addCase(addContact.rejected, handleRejected);
   },
-  // reducers: {
-  //   fetching(state) {
-  //     state.isLoading = true;
-  //   },
-  //   fetchingSuccess(state, { payload }) {
-  //     state.isLoading = false;
-  //     state.contacts = payload;
-  //     state.error = '';
-  //   },
-
-  //   fetchingError(state, { payload }) {
-  //     state.error = payload;
-  //   },
-  // addContact(state, { payload }) {
-  //   if (state.some(contact => contact.name === payload.name)) {
-  //     Notify.failure(`${payload.name}, is already in contact`);
-  //   } else {
-  //     state.push(payload);
-  //   }
-  // },
-  // deleteContact(state, action) {
-  //   const idx = state.findIndex(contact => contact.id === action.payload.id);
-  //   state.splice(idx, 1);
-  // },
 });
 
 export const contactsReducer = contactsSlice.reducer;
-// export const { addContact, deleteContact } = contactsSlice.actions;
-// export const { fetching, fetchingSuccess, fetchingError } =
-//   contactsSlice.actions;
