6fe6eed1fed3f983724119cfec75bf920964754f
NikitaSimakov <106473909+NikitaSimakov@users.noreply.github.com>
2023-06-19 11:30:55 +0300

    add Notify

--- src/redux/contactSlice.js
+++ src/redux/contactSlice.js
@@ -1,7 +1,7 @@
 import { contactsState } from './state';
 import { createSlice } from '@reduxjs/toolkit';
 import { fetchContacts, deleteContact, addContact } from './thunks';
-// import { Notify } from 'notiflix';
+import { Notify } from 'notiflix';
 
 const handlePending = state => (state.isLoading = true);
 const handleRejected = (state, { payload }) => {
@@ -17,22 +17,30 @@ const contactsSlice = createSlice({
         state.contacts = payload;
         state.isLoading = false;
       })
-      .addCase(fetchContacts.pending, handlePending)
-      .addCase(fetchContacts.rejected, handleRejected)
       .addCase(deleteContact.fulfilled, (state, { payload }) => {
         const index = state.contacts.findIndex(
           contact => contact.id === payload
         );
         state.contacts.splice(index, 1);
       })
-      .addCase(deleteContact.pending, handlePending)
-      .addCase(deleteContact.rejected, handleRejected)
       .addCase(addContact.fulfilled, (state, { payload }) => {
+        if (
+          state.contacts.some(contact => contact.name === payload.data.name)
+        ) {
+          Notify.failure(
+            `Contact ${payload.data.name} is already in phonebook!`
+          );
+          return;
+        }
         console.log(payload);
         state.contacts.push(payload.data);
       })
-      .addCase(addContact.pending, handlePending)
-      .addCase(addContact.rejected, handleRejected);
+      .addMatcher(action => {
+        action.type.endsWith('/pendsing');
+      }, handlePending)
+      .addMatcher(action => {
+        action.type.endsWith('/rejected');
+      }, handleRejected);
   },
 });
 
